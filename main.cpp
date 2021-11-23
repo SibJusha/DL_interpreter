5a601d931297ce7e78bbe8fe5ee65355e49a0749
Andrjusha <andreas.yushac@gmail.com>
2021-11-23 14:42:50 +0700

    Here goes the stack (not done yet)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <unordered_map>
 #include <functional>
 #include <stdexcept>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <utility>
+#include <memory>
 
 class eval_error : std::exception {
 public:
@@ -19,6 +24,14 @@ public:
     }
 };
 
+class parse_error : std::exception {
+public:
+    static const char* error() noexcept {
+        return "parse error";
+    }
+};
+
+
 template <typename Returnable>
 class Expression {
     /*...*/
@@ -29,7 +42,6 @@ public:
     [[nodiscard]] virtual int getValue() const = 0;
 
 };
-
 
 class Val : public Expression<Val> {
     int integer;
@@ -42,8 +54,23 @@ public:
         return *this;
     }
 
+    Val& operator= (int n) {
+        *this = Val(n);
+        return *this;
+    }
+
     [[nodiscard]] int getValue() const override {
         return integer;
+    }
+
+    static int parse () {
+        int get;
+        try {
+            std::cin >> get;
+        } catch (...) {
+            throw parse_error();
+        }
+        return get;
     }
 
 };
@@ -52,8 +79,8 @@ class Var : public Expression<Val> {
     std::string id;
 public:
 
-    explicit Var(const std::string& id):
-        id(id)
+    explicit Var(std::string id):
+        id(std::move(id))
     {}
 
     Val eval() override;
@@ -70,50 +97,48 @@ public:
         throw getValue_error();
     }
 
-};
-
-namespace std {
-    template<> struct hash<Var> {
-        std::size_t operator()(const Var &v) const {
-            return hash<std::string>{}(v.get_id());
-        }
-    };
-}
-
-struct Env {
-
-    static std::unordered_map<Var, Val> env;
-
-    static Val fromEnv(const Var &V) {
-        Val found;
-        try {
-            found = env.at(V);
-        } catch (const std::out_of_range& exception) {
-            throw exception;
-        }
-        return found;
+    static std::string parse () {
+        std::string get;
+        std::cin >> get;
+        return get;
     }
 
 };
+
+
+static std::unordered_map<std::string, Expression<Val>*> env;
+static std::unique_ptr<Expression<Val>> fromEnv(const std::string& V) {
+    Expression<Val> *found;
+    try {
+        found = env.at(V);
+    } catch (const std::out_of_range &exception) {
+        throw exception;
+    }
+    return std::unique_ptr<Expression<Val>>(found);
+}
+
 
 Val Var::eval() {
     Val found;
     try {
-        found = Env::fromEnv(*this);
-    } catch (const std::out_of_range& exception) {
-        throw eval_error();
+        found = fromEnv(id)->getValue();
+    } catch (const std::exception& exception) {
+        throw exception;
     }
     return found;
 }
 
+std::unique_ptr<Expression<Val>> Parser();
+
 class Add : public Expression<Val> {
-    Expression* left;
-    Expression* right;
+    std::unique_ptr<Expression<Val>> left;
+    std::unique_ptr<Expression<Val>> right;
 public:
 
-    Add(Expression* left, Expression* right) :
-        left(left),
-        right(right)
+    Add(std::unique_ptr<Expression<Val>> left,
+            std::unique_ptr<Expression<Val>> right) :
+        left(std::move(left)),
+        right(std::move(right))
     {}
 
     Add() : Add(nullptr, nullptr) {}
@@ -121,9 +146,10 @@ public:
     Val eval() override {
         Val result;
         try {
-            result = Val(left->eval().getValue() + right->eval().getValue());
-        } catch (...) {
-            throw eval_error();
+            result = Val(left->eval().getValue() +
+                    right->eval().getValue());
+        } catch (const std::exception& exception) {
+            throw exception;
         }
         return result;
     }
@@ -132,21 +158,31 @@ public:
         throw getValue_error();
     }
 
+    void setLeft(std::unique_ptr<Expression<Val>> left_) {
+        left = std::move(left_);
+    }
+
+    void setRight(std::unique_ptr<Expression<Val>> right_) {
+        right = std::move(right_);
+    }
+
 };
 
 class If : public Expression<Val> {
-    Expression* if_left_;
-    Expression* if_right_;
-    Expression* then_;
-    Expression* else_;
+    std::unique_ptr<Expression<Val>> if_left_;
+    std::unique_ptr<Expression<Val>> if_right_;
+    std::unique_ptr<Expression<Val>> then_;
+    std::unique_ptr<Expression<Val>> else_;
 public:
 
-    If(Expression* if_left_, Expression* if_right_,
-            Expression* then_, Expression* else_) :
-        if_left_(if_left_),
-        if_right_(if_right_),
-        then_(then_),
-        else_(else_)
+    If(std::unique_ptr<Expression<Val>> if_left_,
+       std::unique_ptr<Expression<Val>> if_right_,
+       std::unique_ptr<Expression<Val>> then_,
+       std::unique_ptr<Expression<Val>> else_) :
+        if_left_(std::move(if_left_)),
+        if_right_(std::move(if_right_)),
+        then_(std::move(then_)),
+        else_(std::move(else_))
     {}
 
     If() : If(nullptr, nullptr, nullptr, nullptr) {}
@@ -157,8 +193,8 @@ public:
                 return then_->eval();
             }
             return else_->eval();
-        } catch (...) {
-            throw eval_error();
+        } catch (const std::exception& exception) {
+            throw exception;
         }
     }
 
@@ -169,13 +205,14 @@ public:
 };
 
 class Let : public Expression<Val> {
-    Expression* in;
+    std::unique_ptr<Expression<Val>> in;
 public:
 
-    Let (const std::string& id, int integer, Expression* in) :
-        in(in)
+    Let (const std::string& id, std::unique_ptr<Expression<Val>> id_expr,
+         std::unique_ptr<Expression<Val>> in) :
+        in(std::move(in))
     {
-        Env::env.insert_or_assign(Var(id), Val(integer));
+        env.insert_or_assign(id, id_expr);
     }
 
     Val eval() override {
@@ -194,13 +231,21 @@ public:
 
 };
 
+struct ThisIsFunction : public std::exception {};
+
 class Function : public Expression<Val> {
     std::string id;
     Expression* func_expr;
 public:
 
-    Function() :
+    Function(std::string  id, Expression* func_expr) :
+        id(std::move(id)),
+        func_expr(func_expr)
+    {}
 
+    Val eval() override {
+        throw ThisIsFunction();
+    }
 
     [[nodiscard]] int getValue() const override {
         throw getValue_error();
@@ -208,9 +253,151 @@ public:
 
 };
 
+class Call : public Expression<Val> {
+    std::string arg_id;
+    Expression* arg_expression;
+public:
 
+    Val eval() override {
+        Val result;
+        try {
+            arg_expression->eval();
+        } catch (const ThisIsFunction& yes) {
+
+        }
+        throw eval_error();
+    }
+
+};
+
+/*
+ * (add (val 3) (val 2))
+ *
+ * add val 3 val 2
+ *
+ * let A = val 20 in
+ * let B = val 30 in
+ * if var A add var B val 3
+ * then val 10
+ * else add var B val 1
+ *
+ * (let A = (val 20) in
+ * (let B = (val 30) in
+ * (if (var A) (add (var B) (val 3))
+ * then (val 10)
+ * else (add (var B) (val 1)))))
+ */
+
+std::string RemoveUnnecessary_and_GetName(std::string& str) {
+    std::string result;
+    for (auto ch : str)
+    {
+        if (ch == '(' || ch == ' ' || ch == ')') {
+            continue;
+        } else {
+            result += ch;
+        }
+    }
+    return result;
+}
+
+std::unique_ptr<Expression<Val>> Parser() {
+    std::string current, input;
+    while (current.empty()) {
+        std::cin >> input;
+        current = RemoveUnnecessary_and_GetName(input);
+    }
+    try {
+        if (current == "val") {
+            std::unique_ptr<Expression<Val>> result = std::make_unique<Val>(Val(Val::parse())); //add to ProgramStack maybe?
+            return result;
+        } else if (current == "var") {
+            std::unique_ptr<Expression<Val>> result(new Var(Var::parse()));
+            return result;
+        } else if (current == "add") {
+            auto temp = new Add();
+            temp->setLeft(Parser());
+            temp->setRight(Parser());
+            std::unique_ptr<Expression<Val>> result(temp);
+            return result;
+        }
+    } catch (const std::exception& exception) {
+        throw exception;
+    }
+}
+
+/* DumbHash(Expression_Name):
+ * val = 111
+ * var = 117
+ * add = 103
+ * if = 72
+ * let = 119
+ * function = 118
+ * call = 112
+ */
+
+enum typeInHash {val = 111, var = 117, add = 103, _if = 72, let = 119,
+        function = 118, call = 112};
+size_t DumbHash (const std::string& str) {
+    return str.length() + str.back();
+}
+
+std::unique_ptr<Expression<Val>> Read_and_Create() {
+
+    std::stack<std::unique_ptr<Expression<Val>>> ParseStack;
+    std::stack<typeInHash> TypeStack;
+    std::unique_ptr<Expression<Val>> result;
+
+    std::string current, input;
+    while (current.empty() && !input.empty()) {
+        std::cin >> input;
+        current = RemoveUnnecessary_and_GetName(input);
+    }
+    while (!input.empty()){
+        auto cur = static_cast<typeInHash>(DumbHash(current));
+        try {
+            switch (cur) {
+                case val:
+                    result = std::make_unique<Val>(Val::parse());
+                    ParseStack.push(result);
+                    TypeStack.push(val);
+                    continue;
+                case var:
+                    continue;
+                case add: {
+                    auto temp = new Add();
+                    std::unique_ptr<Expression<Val>> result_(temp);
+                    result = std::move(result_);
+                    ParseStack.push(result);
+                    TypeStack.push(add);
+                    continue;
+                }
+                case _if:
+                    break;
+                case let:
+                    break;
+                case function:
+                    break;
+                case call:
+                    break;
+            }
+        } catch (const std::exception &exception) {
+            throw exception;
+        }
+    }
+
+    while (!ParseStack.empty()) {
+
+    }
+
+    return result;
+}
 
 int main() {
-
+    try {
+        std::cout << Read_and_Create()->eval().getValue();
+    } catch (...) {
+        std::cout << "ERROR";
+    }
     return 0;
 }
